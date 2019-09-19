a78bbc997842219fee7964f924865b98c8f54138
Ali Shah <ali1195shah@gmail.com>
2019-09-19 12:32:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,9 +8,8 @@ class Dog
     @this_dogs_name
   end
   
-  lassie = Dog.new
-lassie.name = "Lassie"
- 
-lassie.name #=> "Lassie"
-  
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.name #=> "Lassie"
